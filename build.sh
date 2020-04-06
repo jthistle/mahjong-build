0fa88d2e06f91bc2744fa97fda3f9facdb403090
James Thistlewood <jamesthistlewood@gmail.com>
2020-04-06 20:34:08 +0100

    attempt it all again

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+
 mv ./mahjong-front ./mahjong-back/client/
 cd ./mahjong-back/client/
 yarn build
-rm .git/ -rf
 
 # Prepare for deploy
+shopt -s dotglob nullglob
+rm .git/ -rf
 cd ../
 rm .git/ -rf
 cd ../
@@ -13,6 +15,4 @@ rm .git/ -rf
 rm .gitmodules
 mv ./mahjong-back/* .
 rm -rf mahjong-back
-
-git init
-git commit -am "initial commit"
+shopt -u dotglob nullglob

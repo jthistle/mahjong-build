d011f6325378910c8bd0dc3dca621028f290f83f
James Thistlewood <jamesthistlewood@gmail.com>
2020-04-06 23:00:10 +0100

    this probably won't work

--- build.sh
+++ build.sh
@@ -5,13 +5,11 @@ yarn build
 cd ../
 
 mv ./mahjong-front/build ./mahjong-front/client
-cp ./mahjong-front/client ./mahjong-back/src/
+cp ./mahjong-front/client ./mahjong-back/src/ -r
 rm -rf ./mahjong-front
 
 # Prepare for deploy
 shopt -s dotglob nullglob
-rm .git/ -rf
-cd ../
 rm .git/ -rf
 rm .gitmodules
 mv ./mahjong-back/* .
